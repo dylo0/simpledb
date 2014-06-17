a7c0d3f60c306af950debd6052a787d73a7bc7dd
Tomasz Dyl <tomekdyl@gmail.com>
2014-06-17 19:08:35 +0200

    added simple table to view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the simpledbApp
  */
 angular.module('simpledbApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.elements = [{
+    	time: '123',
+    	name: 'asd',
+    	value: 'val'
+    	},
+    	{
+    	time: '433',
+    	name: 'asd',
+    	value: 'val'
+    	},
+    	{
+    	time: '673',
+    	name: 'asd',
+    	value: 'val'
+    	}
+    ]
+
   });
+
